9e9766f7811eb87109f6835921366ee52c34478f
J B <ironviralmedia@gmail.com>
2018-02-01 23:38:20 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language()
+def introduction_with_language(name, language)
+  puts "Hi, my name is Dan and I am learning to program in #{language}."
